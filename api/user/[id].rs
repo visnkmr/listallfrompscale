7209575175b1407120ad0fa3fd5c466b754f552a
12core1 <12533753+visnkmr@users.noreply.github.com>
2023-11-27 20:56:53 +0530

    update file names and var names

--- api/user/[id].rs
+++ api/user/[id].rs
@@ -1,6 +1,9 @@
 use serde::Serialize;
+use serde_json::json;
 use std::collections::HashMap;
 use url::Url;
+use listallfrompscale::{ printeuser,adddatatouser};
+
 use vercel_runtime::{http::bad_request, run, Body, Error, Request, Response, StatusCode};
 
 #[derive(Serialize)]
@@ -17,18 +20,31 @@ async fn main() -> Result<(), Error> {
 pub async fn handler(req: Request) -> Result<Response<Body>, Error> {
     let parsed_url = Url::parse(&req.uri().to_string()).unwrap();
     let hash_query: HashMap<String, String> = parsed_url.query_pairs().into_owned().collect();
-    let id_key = hash_query.get("id");
+    let uid = hash_query.get("id");
 
-    match id_key {
+    match uid {
         None => {
             return bad_request(APIError {
                 message: "Query string is invalid",
                 code: "query_string_invalid",
             });
         }
-        Some(id) => Ok(Response::builder()
+        Some(id) => {
+            let data=printeuser(id.to_string(), "".to_string()).unwrap().url;
+            let jdata:Vec<String>=serde_json::from_str(&data).unwrap();
+            Ok(Response::builder()
             .status(StatusCode::OK)
             .header("Content-Type", "application/json")
-            .body(Body::Text(id.to_owned()))?),
+            // .body(Body::Text(id.to_owned()))?),
+            .body(
+                json!({
+                // "got": format!("added {} to {}!", payload.uid, payload.datatoadd),
+                "data": serde_json::to_string(&jdata).unwrap(),
+                "request":format!("{:?}",req),
+                })
+                .to_string()
+                .into(),
+            )?)
+        }
     }
 }
