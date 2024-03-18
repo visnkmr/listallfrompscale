c4f0f01ceab64f81b3ed13966b95f88f22ffad6b
12coreub <12533753+visnkmr@users.noreply.github.com>
2024-03-19 01:22:00 +0530

    temporarily enable aiven

--- src/lib.rs
+++ src/lib.rs
@@ -20,10 +20,10 @@ use mysql::{params, prelude::Queryable, Params, Pool, PooledConn, QueryResult, R
 use serde::*;
 
 pub fn getconn(url:String)->Pool{
-    let mut ssl_opts = SslOpts::default();
-    let ca_cert=env::var("CA_CERT").unwrap();
-    let dec:String=serde_json::from_str(&ca_cert).unwrap();
-    ssl_opts = ssl_opts.with_root_cert_path(Some((&dec.clone())));
+    let mut ssl_opts = SslOpts::default().with_danger_accept_invalid_certs(true);
+    // let ca_cert=env::var("CA_CERT").unwrap();
+    // let dec:String=serde_json::from_str(&ca_cert).unwrap();
+    // ssl_opts = ssl_opts.with_root_cert_path(Some((&dec.clone())));
     
     let builder = mysql::OptsBuilder::from_opts(mysql::Opts::from_url(&url).unwrap()).ssl_opts(ssl_opts.clone());
 
@@ -136,6 +136,7 @@ fn parse_value_from_data(mut row: mysql::Row) -> eachredisentry {
 #[test]
 fn trydbcon(){
     dotenv().ok();
+    createtable();
 //     let file_contents = fs::read_to_string("./ca.pem")
 //     .expect("Should have been able to read the file");
 // let file_contents=serde_json::to_string(&file_contents).unwrap();
@@ -147,25 +148,25 @@ fn trydbcon(){
     // if(ab.exists()){
         // // let uname="vis".to_string();
         // // let pwd="example".to_string();
-                        // let uname="345".to_string();
+                        let uname="345".to_string();
                         // // // let data=printeuser("ram".to_string(),String::new()).unwrap().url;
                         // // // let jdata:Vec<String>=serde_json::from_str(&data).unwrap();
                         // // // println!("{:?}",jdata);
                         // // // println!("{:?}",data);
                         // // // println!("{:?}",createuser("vis".to_string(), "example".to_string()));
                         // // // println!("{:?}",deleteuser("meg".to_string(), "example".to_string()));
-                        // // let sdp_offer = json!({
-                        // //     "type": "offer",
-                        // //     "sdp": "v=0\r\no=- 8748985181318156403 2 IN IP4 127.0.0.1\r\ns=-\r\nt=0 0\r\na=group:BUNDLE 0\r\na=extmap-allow-mixed\r\na=msid-semantic: WMS\r\nm=application 43271 UDP/DTLS/SCTP webrtc-datachannel\r\nc=IN IP4 117.207.43.69\r\na=candidate:1766165686 1 udp 2113937151 793f23b1-475f-4f64-b97e-a44daeba377e.local 43271 typ host generation 0 network-cost 999\r\na=candidate:4156233652 1 udp 1677729535 117.207.43.69 43271 typ srflx raddr 0.0.0.0 rport 0 generation 0 network-cost 999\r\na=ice-ufrag:EV3N\r\na=ice-pwd:aEfq1TO9GBLyyET0xXzZXug5\r\na=fingerprint:sha-256 82:5F:DD:D3:5A:BE:17:9F:9D:66:EC:3E:BA:FU:CU:17:20:8E:CD:13:89:E3:8B:5C:55:AE:87:A2:25:D4:19:AA\r\na=setup:actpass\r\na=mid:0\r\na=sctp-port:5000\r\na=max-message-size:262144\r\n"
-                        // //  });
-                        // //  let tojson=serde_json::to_string(&sdp_offer).unwrap();
-                        // // //  print!("{:?}",tojson);
-                        // // let ddata=addtoquickfetch("345".to_string(),tojson ).unwrap();
-                        // // print!("{:?}",ddata);
-                        // let data=getfromquickfetch(uname).unwrap().value;
-                        // println!("{:?}",data);
-        // let jdata:Value=serde_json::from_str(&data).unwrap();
-        // println!("{:?}",jdata);
+                        let sdp_offer = json!({
+                            "type": "offer",
+                            "sdp": "v=0\r\no=- 8748985181318156403 2 IN IP4 127.0.0.1\r\ns=-\r\nt=0 0\r\na=group:BUNDLE 0\r\na=extmap-allow-mixed\r\na=msid-semantic: WMS\r\nm=application 43271 UDP/DTLS/SCTP webrtc-datachannel\r\nc=IN IP4 117.207.43.69\r\na=candidate:1766165686 1 udp 2113937151 793f23b1-475f-4f64-b97e-a44daeba377e.local 43271 typ host generation 0 network-cost 999\r\na=candidate:4156233652 1 udp 1677729535 117.207.43.69 43271 typ srflx raddr 0.0.0.0 rport 0 generation 0 network-cost 999\r\na=ice-ufrag:EV3N\r\na=ice-pwd:aEfq1TO9GBLyyET0xXzZXug5\r\na=fingerprint:sha-256 82:5F:DD:D3:5A:BE:17:9F:9D:66:EC:3E:BA:FU:CU:17:20:8E:CD:13:89:E3:8B:5C:55:AE:87:A2:25:D4:19:AA\r\na=setup:actpass\r\na=mid:0\r\na=sctp-port:5000\r\na=max-message-size:262144\r\n"
+                         });
+                         let tojson=serde_json::to_string(&sdp_offer).unwrap();
+                        //  print!("{:?}",tojson);
+                        let ddata=addtoquickfetch("345".to_string(),tojson ).unwrap();
+                        print!("{:?}",ddata);
+                        let data=getfromquickfetch(uname).unwrap().value;
+                        println!("{:?}",data);
+        let jdata:Value=serde_json::from_str(&data).unwrap();
+        println!("{:?}",jdata);
 
         // println!("{:?}",adddatatouser("vis".to_string(), "google.com".to_string()));
         // println!("{:?}",printdata());
